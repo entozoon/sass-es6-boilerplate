9fea5c8936903f2e9a34b67441cec3671060ec1b
Michael Cook <michael.cook@techdept.co.uk>
2017-08-31 13:04:24 +0100

    Lovely little Sass/ES6 compiler set up for a static site

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+[1, 2, 3, 4].map(function (i) {
+  console.log('Testing ES6 compilation, four times. #' + i);
+});
---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+[1,2,3,4].map(i => {
+  console.log('Testing ES6 compilation, four times. #' + i);
+});
